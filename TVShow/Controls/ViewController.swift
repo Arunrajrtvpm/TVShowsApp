85a92302f40205bf1cdb3821b3d9e3dcf792e605
ARUNRAJ <arunraj.r@zlinkcorp.com>
2021-08-04 18:42:14 +0530

    initial setup

---
+++ TVShow/Controls/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TVShow
+//
+//  Created by Arunraj on 04/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "SHOWS"
+        
+        
+    }
+
+
+}
+
